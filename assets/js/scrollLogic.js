87ae414184d74b453d7b6618a46f38747e22605e
Dave <davefunk135@gmail.com>
2021-01-02 21:12:52 -0800

    detecting position on the page responsively

--- assets/js/scrollLogic.js
+++ assets/js/scrollLogic.js
@@ -1,16 +1,48 @@
+//selectors;
+const sectionOne = document.querySelector('section.section.section__1');
+const sectionTwo = document.querySelector('section.section.section__2');
+const sectionThree = document.querySelector('section.section.section__3');
+const sectionFour = document.querySelector('section.section.section__4');
 
-function changeBackgroundColor() {
-    // this value is the distance from the top of the window to this element.
-    // this value changes depending on your viewport position.
-    const section1Top = document.querySelector('section.section.section__1').getBoundingClientRect().top;
+// references to top of elements in selectors
+const sectionOneTop = sectionOne.getBoundingClientRect().top;
+const sectionTwoTop = sectionTwo.getBoundingClientRect().top;
+const sectionThreeTop = sectionThree.getBoundingClientRect().top;
+const sectionFourTop = sectionFour.getBoundingClientRect().top;
 
-    //presumably y coordinate position from page top?
-    const scrollPosition = window.pageYOffset;
-    console.log({ scrollPosition, section1Top });
+// height of document in pixels
+const height = document.body.scrollHeight;
 
+// height refs, unecessary, but makes me feel safer than 
+// dividing total height by four
+const sectionHeight = height / 4;
+const sectionOneHeight = sectionOne.getClientRects()[0].height;
+const sectionTwoHeight = sectionOne.getClientRects()[0].height;
+const sectionThreeHeight = sectionOne.getClientRects()[0].height;
+const sectionFourHeight = sectionOne.getClientRects()[0].height;
 
-}
+//wherever theres and end to a section, we'll subtract a quarter of that
+// sections height to get our animation trigger. 
+const padding = sectionHeight / 4;
 
-window.addEventListener('scroll', function (e) {
-    changeBackgroundColor();
+//heres where we trigger the animations
+const sectionOneTriggerLocation = sectionHeight - padding;
+const sectionTwoTriggerLocation = (sectionHeight * 2) - padding;
+const sectionThreeTriggerLocation = (sectionHeight * 3) - padding;
+const sectionFourTriggerLocation = (sectionHeight * 4) - padding;
+
+window.addEventListener('scroll', function () {
+    const currentY = window.pageYOffset;
+
+    if (currentY >= sectionOneTriggerLocation && currentY < sectionHeight) {
+        console.log('section 1');
+    }
+
+    if (currentY >= sectionTwoTriggerLocation && currentY < (sectionHeight * 2)) {
+        console.log('section 2');
+    }
+
+    if (currentY >= sectionThreeTriggerLocation && currentY < (sectionHeight * 3)) {
+        console.log('section 3');
+    }
 });
