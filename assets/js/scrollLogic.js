9572736b596f458064dc5fe1e7539efd9d27c8ea
Dave <davefunk135@gmail.com>
2021-01-04 01:31:10 -0800

    cleaned up code and css, now works dynamically! removed class code as that was unecessary. removed a few redundant areas and removed some unused code. I think I've refactored this to a reasonable amount. Next up is animations for content placement

--- assets/js/scrollLogic.js
+++ assets/js/scrollLogic.js
@@ -10,26 +10,6 @@ const colors = ['rgb(32, 65, 142)', 'rgb(39, 41, 50)', 'rgb(245, 143, 41)', 'rgb
 // height of entire document in pixels
 const height = document.body.scrollHeight;
 
-// just easier to set up sections this way. also might be handy for infividual and all properties.
-class Section {
-    constructor(sectionColor,
-        sectionNextColor,
-        sectionTrigger,
-        sectionNumber,
-        sectionStart,
-        sectionEnd,
-        sectionTransitionScale
-    ) {
-        this.sectionColor = sectionColor;
-        this.sectionNextColor = sectionNextColor;
-        this.sectionTrigger = sectionTrigger;
-        this.sectionNumber = sectionNumber;
-        this.sectionStart = sectionStart;
-        this.sectionEnd = sectionEnd;
-        this.sectionTransitionScale = sectionTransitionScale;
-    }
-}
-
 // height ref. int is number of sections. (this will be subbed with sections array.length)
 const sectionHeight = height / sectionSelectors.length;
 
@@ -38,54 +18,57 @@ const padding = sectionHeight / 3;
 
 //on page load we create our sections from the markup.
 function generateSections() {
-    if (!sectionSelectors) {
-        console.error('no sections found.');
-    }
+    sectionSelectors.forEach((_, i) => {
+        //implement random colors by randomizing array itself, then padding those in.
 
-    sectionSelectors.forEach((section, i) => {
-        //implement random colors by randomizing array itself, then padding those in.;
         const sectionColor = colors[i];
         const sectionNextColor = colors[i + 1] || 'rgb(255, 255, 255)';
         const sectionTrigger = sectionHeight * (i + 1) - padding;
         const sectionNumber = i + 1;
         const sectionStart = sectionHeight * i;
         const sectionEnd = sectionHeight * (i + 1);
-        const sectionTransitionScale = chroma.scale([sectionColor, sectionNextColor]).domain([sectionTrigger, sectionEnd]);
+        const sectionTransitionScale = chroma.scale([sectionColor, sectionNextColor])
 
-        const newSection = new Section(sectionColor, sectionNextColor, sectionTrigger, sectionNumber, sectionStart, sectionEnd, sectionTransitionScale);
+        //set range of color scale to be between the pixel ranges we need it to be depending on the section
+        sectionTransitionScale.domain([sectionTrigger, sectionEnd]);
+
+        const newSection = {
+            sectionColor,
+            sectionNextColor,
+            sectionTrigger,
+            sectionNumber,
+            sectionStart,
+            sectionEnd,
+            sectionTransitionScale
+        };
+
         sectionCollection.push(newSection);
     });
 
 }
 
-// this function will be called by an event listener boud to scrolling. It will iterate through the sections 
-// tofigure out where we are and what color our background should be;
+// this function will be called by an event listener boud to scrolling.
 function changeBackgroundColor() {
     const currentY = window.pageYOffset;
 
-    sectionCollection.map((section, i) => {
-        console.log({
-            currentY,
-            start: section.sectionStart,
-            end: section.sectionEnd,
-            trigger: section.sectionTrigger
-        });
+    sectionCollection.map((section) => {
+        if (currentY <= section.sectionEnd) {
+            if (currentY >= section.sectionTrigger) {
+                return document.body.style.backgroundColor = section.sectionTransitionScale(currentY);
+            }
 
-        if (currentY <= section.sectionEnd && currentY >= section.sectionTrigger) {
-            console.log('hit');
-            const scale = chroma.scale([section.sectionColor, section.sectionNextColor]).domain([section.sectionTrigger, section.sectionEnd]);
-
-            return document.body.style.backgroundColor = scale(currentY);
+            if (currentY >= section.sectionStart && currentY < section.sectionTrigger) {
+                return document.body.style.backgroundColor = section.sectionColor;
+            }
         }
-
     });
-
 };
 
+//run these on pageload to set up dom.
 generateSections();
-
 changeBackgroundColor();
 
+//attach a listener to the scroll event to refire the background changing function
 window.addEventListener('scroll', function () {
     changeBackgroundColor();
 });
