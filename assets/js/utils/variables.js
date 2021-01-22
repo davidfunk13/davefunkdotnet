a40ccebf417157d5688ccdca151dab3eb19e47f4
Dave <davefunk135@gmail.com>
2021-01-21 23:14:16 -0800

    large improvements to typeface and responsiveness

--- assets/js/utils/variables.js
+++ assets/js/utils/variables.js
@@ -2,15 +2,11 @@
 const sectionCollection = [];
 
 //array of colors for each section, will possibly randomize these and add extras.
-const colors = ['rgb(32, 65, 142)', 'rgb(39, 41, 50)', 'rgb(245, 143, 41)', 'rgb(130, 132, 137)'];
+const colors = ['rgb(32, 65, 142)', 'rgb(39, 41, 50)', 'rgb(32, 40, 64)', 'rgb(130, 132, 137)'];
 
 // height of entire document in pixels
 // const height = document.body.scrollHeight;
 const height = (document.height !== undefined) ? document.height : document.body.offsetHeight;
 // console.log({ height })
 
-// // height ref. int is number of sections. (this will be subbed with sections array.length)
-const sectionHeight = height / sectionSelectors.length;
-
-// // isolate the last third of a given sections height, or half or quarter whatever in pixels. 
-const padding = sectionHeight / 3;
+// rgb(82, 91, 118)
